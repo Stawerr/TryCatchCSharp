2c235014890031a3f32a25597223eea8079a612d
Stawerr <renato-silva_16@hotmail.com>
2022-03-08 17:20:41 +0000

    Add project files.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0; 
+            bool flag = false;
+            Console.WriteLine("Digite o montante:"); 
+            do
+            {
+                try
+                {
+                    x = double.Parse(Console.ReadLine()); 
+                    flag = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message); 
+                }
+            } while (!flag);
+        }
+    }
+}
